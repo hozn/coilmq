414ac88d966ffb8bfa2c1225ae8c3aac00559cc2
Hans Lellelid <hans@xmpl.org>
2010-02-19 09:42:19 -0500

    Updating version to reflect in-development state (0.3dev)

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except ImportError:
 
 setup(
     name='CoilMQ',
-    version='0.2',
+    version='0.3',
     description='STOMP message broker',
     long_description=__doc__,
     keywords='stomp server broker',
