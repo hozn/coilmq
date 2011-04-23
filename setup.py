2706f5fa29cc60c54d0ede6717c395e88441bb0e
Hans Lellelid <hans@xmpl.org>
2011-04-23 10:58:14 -0400

    Bumping version for next release.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-version = '0.4.3'
+version = '0.4.4'
 
 news = os.path.join(os.path.dirname(__file__), 'docs', 'news.txt')
 news = open(news).read()
