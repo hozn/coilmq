6916cba0c0cae8c7b17db4510b5a03fca4b8328d
Hans Lellelid <hans@xmpl.org>
2011-06-28 20:28:39 -0400

    Bumping version and adding news.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
-version = '0.5.1b1'
+version = '0.6.0'
 
 news = os.path.join(os.path.dirname(__file__), 'docs', 'news.txt')
 news = open(news).read()
