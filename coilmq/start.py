1737afd75e2733c556e65a5fab9e3dfef5de2ad3
ZhukovAlexander <zhukovaa90@gmail.com>
2015-12-11 10:16:33 +0000

    Switch to python click

--- coilmq/start.py
+++ coilmq/start.py
@@ -11,11 +11,13 @@ from optparse import OptionParser
 
 try:
     daemon_support = True
-    import daemon
+    import daemon as pydaemon
     import signal
     import lockfile
 except ImportError:
     daemon_support = False
+
+import click
 
 from coilmq.config import config as global_config, init_config, init_logging, resolve_name
 from coilmq.topic import TopicManager
@@ -38,7 +40,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License."""
 
-logger = lambda: logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
 
 
 def server_from_config(config=None, server_class=None, additional_kwargs=None):
@@ -85,11 +87,11 @@ def server_from_config(config=None, server_class=None, additional_kwargs=None):
                                                             queue_scheduler=queue_scheduler_factory()),
                                  topic_manager=TopicManager(),
                                  authenticator=authenticator)
-    logger().info("Created server:%r" % server)
+    logger.info("Created server:%r" % server)
     return server
 
 
-def context_serve(options, context):
+def context_serve(context, configfile, listen_addr, listen_port, logfile, debug, daemon, uid, gid, pidfile, umask, rundir):
     """
     Takes a context object, which implements the __enter__/__exit__ "with" interface 
     and starts a server within that context.
@@ -116,19 +118,19 @@ def context_serve(options, context):
             # AND we're in a daemon context, then we're not going to be able to do anything with it.
             # We've got no stderr/stdout here; and so (to my knowledge) no reliable (& cross-platform),
             # way to display errors.
-            level = logging.DEBUG if options.debug else logging.INFO
-            init_logging(logfile=options.logfile, loglevel=level,
-                         configfile=options.configfile)
+            level = logging.DEBUG if debug else logging.INFO
+            init_logging(logfile=logfile, loglevel=level,
+                         configfile=configfile)
 
             server = server_from_config()
-            logger().info("Stomp server listening on %s:%s" % server.server_address)
+            logger.info("Stomp server listening on %s:%s" % server.server_address)
 
-            if options.debug:
+            if debug:
                 poll_interval = float(global_config.get(
                     'coilmq', 'debug.stats_poll_interval'))
                 if poll_interval:  # Setting poll_interval to 0 effectively disables it.
                     def diagnostic_loop(server):
-                        log = logger()
+                        log = logger
                         while True:
                             log.debug(
                                 "Stats heartbeat -------------------------------")
@@ -149,97 +151,42 @@ def context_serve(options, context):
             server.serve_forever()
 
     except (KeyboardInterrupt, SystemExit):
-        logger().info("Stomp server stopped by user interrupt.")
+        logger.info("Stomp server stopped by user interrupt.")
         raise SystemExit()
     except Exception as e:
-        logger().error("Stomp server stopped due to error: %s" % e)
-        logger().exception(e)
+        logger.error("Stomp server stopped due to error: %s" % e)
+        logger.exception(e)
         raise SystemExit()
     finally:
         if server:
             server.server_close()
 
 
-def main():
-    """
-    Main entry point for running a socket server from the commandline.
-
-    This method will read in options from the commandline and call the L{config.init_config} method
-    to get everything setup.  Then, depending on whether deamon mode was specified or not, 
-    the process may be forked (or not) and the server will be started.
-    """
-
-    parser = OptionParser()
-    parser.add_option("-c", "--config", dest="configfile",
-                      help="Read configuration from FILE. (CLI options override config file.)", metavar="FILE")
-
-    parser.add_option("-b", "--host", dest="listen_addr",
-                      help="Listen on specified address (default 127.0.0.1)", metavar="ADDR")
-
-    parser.add_option("-p", "--port", dest="listen_port",
-                      help="Listen on specified port (default 61613)", type="int", metavar="PORT")
-
-    parser.add_option("-l", "--logfile", dest="logfile",
-                      help="Log to specified file (unless logging configured in config file).", metavar="FILE")
-
-    parser.add_option("--debug", action="store_true", dest="debug", default=False,
-                      help="Sets logging to debug (unless logging configured in config file).")
-
-    parser.add_option("-d", "--daemon", action="store_true", dest="daemon", default=False,
-                      help="Run server as a daemon (default False).")
-
-    parser.add_option("-u", "--uid", dest="uid",
-                      help="The user/UID to use for daemon process.", metavar="UID")
-
-    parser.add_option("-g", "--gid", dest="gid",
-                      help="The group/GID to use for daemon process.", metavar="GID")
-
-    parser.add_option("--pidfile", dest="pidfile",
-                      help="The PID file to use.", metavar="FILE")
-
-    parser.add_option("--umask", dest="umask",
-                      help="Umask (octal) to apply for daemonized process.", metavar="MASK")
-
-    parser.add_option('--rundir', dest="rundir",
-                      help="The working directory to use for the daemonized process (default /).", metavar="DIR")
-
-    (options, args) = parser.parse_args()
+def _main(config=None, host=None, port=None, logfile=None, debug=None,
+          daemon=None, uid=None, gid=None, pidfile=None, umask=None, rundir=None):
 
     # Note that we must initialize the configuration before we enter the context
     # block; however, we _cannot_ initialize logging until we are in the context block
     # (so we defer that until the context_serve call.)
-    init_config(options.configfile)
+    init_config(config)
 
-    if options.listen_addr is not None:
-        global_config.set('coilmq', 'listen_addr', options.listen_addr)
+    if host is not None:
+        global_config.set('coilmq', 'listen_addr', host)
 
-    if options.listen_port is not None:
-        global_config.set('coilmq', 'listen_port', str(options.listen_port))
+    if port is not None:
+        global_config.set('coilmq', 'listen_port', str(port))
 
-    if options.daemon:
+    if daemon:
         if not daemon_support:
             raise ConfigError(
                 "This environment/platform does not support running as daemon. (Are you running on *nix and did you install python-daemon package?)")
         else:
 
-            context = daemon.DaemonContext()
-
-            if options.uid is not None:
-                context.uid = options.uid
-
-            if options.gid is not None:
-                context.gid = options.gid
-
-            if options.pidfile is not None:
-                context.pidfile = lockfile.FileLock(options.pidfile)
-
-            if options.umask is not None:
-                context.umask = int(options.umask, 8)
-
-            if options.rundir is not None:
-                context.working_directory = options.rundir
-
-            context_serve(options, context)
+            context = pydaemon.DaemonContext(uid=uid,
+                                             gid=gid,
+                                             pidfile=lockfile.FileLock(pidfile) if pidfile else None,
+                                             umask=int(umask, 8),
+                                             working_directory=rundir)
 
     else:
         # Non-daemon mode, so we use a dummy context objectx
@@ -253,7 +200,34 @@ def main():
             def __exit__(self, type, value, traceback):
                 pass
 
-        context_serve(options, DumbContext())
+        context = DumbContext()
+
+    context_serve(context, config, host, port, logfile, debug, daemon, uid, gid, pidfile, umask, rundir)
+
+
+@click.command()
+@click.option("-c", "--config", help="Read configuration from FILE. (CLI options override config file.)", metavar="FILE")
+@click.option("-b", "--host", help="Listen on specified address (default 127.0.0.1)", metavar="ADDR")
+@click.option("-p", "--port", help="Listen on specified port (default 61613)", type=int, metavar="PORT")
+@click.option("-l", "--logfile", help="Log to specified file (unless logging configured in config file).", metavar="FILE")
+@click.option("--debug", default=False, help="Sets logging to debug (unless logging configured in config file).")
+@click.option("-d", "--daemon", default=False, help="Run server as a daemon (default False).")
+@click.option("-u", "--uid", help="The user/UID to use for daemon process.", metavar="UID")
+@click.option("-g", "--gid", help="The group/GID to use for daemon process.", metavar="GID")
+@click.option("--pidfile",   help="The PID file to use.", metavar="FILE")
+@click.option("--umask", help="Umask (octal) to apply for daemonized process.", metavar="MASK")
+@click.option('--rundir', help="The working directory to use for the daemonized process (default /).", metavar="DIR")
+def main(config, host, port, logfile, debug, daemon, uid, gid, pidfile, umask, rundir):
+    """
+    Main entry point for running a socket server from the commandline.
+
+    This method will read in options from the commandline and call the L{config.init_config} method
+    to get everything setup.  Then, depending on whether deamon mode was specified or not, 
+    the process may be forked (or not) and the server will be started.
+    """
+
+    _main(**locals())
+
 
 if __name__ == '__main__':
     try:
@@ -261,5 +235,5 @@ if __name__ == '__main__':
     except (KeyboardInterrupt, SystemExit):
         pass
     except Exception as e:
-        logger().error("Server terminated due to error: %s" % e)
-        logger().exception(e)
+        logger.error("Server terminated due to error: %s" % e)
+        logger.exception(e)
