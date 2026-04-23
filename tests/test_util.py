05f49fab87c7741daa57d7568ec3961dea1a084d
Eric Dramstad <edrams@gmail.com>
2026-04-23 13:26:16 -0400

    test_periodic_callback uses Mock to count callback calls

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,30 +1,18 @@
-import threading
 import time
+from unittest.mock import Mock
 
 from coilmq.util.concurrency import CoilThreadingTimer
 
 
 class TestCoilThreadingTimer:
-    def setup_method(self, method):
-        class CountedCallback:
-            def __init__(self):
-                self.lock = threading.Lock()
-                self.n_called = 0
-
-            def __call__(self, *args, **kwargs):
-                with self.lock:
-                    self.n_called += 1
-
-        self.counter = CountedCallback
-
     def test_periodic_callback(self):
         period = 0.1
         factor = 10
-        counter = self.counter()
+        counter = Mock()
         timer = CoilThreadingTimer()
         timer.schedule(period, counter)
         with timer:
             time.sleep(period * factor)
-        assert abs(counter.n_called - factor) < factor * 0.5, (
+        assert abs(counter.call_count - factor) < factor * 0.5, (
             "Should provide 50% accuracy"
         )
